944934a017e3bd0dfd949f905db9b8b7d71455a2
vaishnavishasank <83600836+vaishnavishasank@users.noreply.github.com>
2022-07-20 12:20:28 -0400

    Add files via upload
    initial changes

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, Fragment} from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
+
+function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
+    </Fragment>
+  );
+}
+
+export default App;
